8a418777dad941986cb5303ec9999d9a340e0e66
Adam Hajro <ada.hajro@gmail.com>
2023-12-07 22:46:43 +0100

    Initial commit.

---
+++ src/main/java/sk/uniza/fri/vaadinapp/VaadinAppApplication.java
@@ -0,0 +1,13 @@
+package sk.uniza.fri.vaadinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/uniza/fri/vaadinapp/VaadinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.uniza.fri.vaadinapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
